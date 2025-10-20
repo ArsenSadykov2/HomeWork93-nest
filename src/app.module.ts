e809fec231e5065d77dee3e3ed5d6bcbbc24f4bb
Sadykov Arsen <LoCo10291998@gmail.com>
2025-10-20 16:50:20 +0600

    HW-94: Created login controller for user.

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { Album, AlbumSchema } from './schemas/album.schema';
 import { Tracks, TracksSchema } from './schemas/track.schema';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users/users.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [
@@ -28,6 +29,6 @@ import { UsersController } from './users/users.controller';
     TracksController,
     UsersController,
   ],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { RegisterUserDTO } from './register-user.dto';
+import { LoginUserDto } from './login-user.dto';
+import { AuthService } from '../auth/auth.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private authService: AuthService,
+  ) {}
 
   @Post()
   registerUser(@Body() userData: RegisterUserDTO) {
@@ -18,5 +23,15 @@ export class UsersController {
 
     user.generateToken();
     return user.save();
+  }
+
+  @Post('sessions')
+  async login(@Body() userData: LoginUserDto) {
+    const { username, password } = userData;
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
